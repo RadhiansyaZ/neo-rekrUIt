0935d6c845bf2c1772e7747ba842513e4313c1d2
Radhiansya Zain Antriksa Putra <radhi.rz666@gmail.com>
2022-07-29 11:43:03 +0700

    Initial Commit

---
+++ src/main/java/com/apb15/neorekruit/NeoRekruitApplication.java
@@ -0,0 +1,13 @@
+package com.apb15.neorekruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoRekruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoRekruitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apb15/neorekruit/NeoRekruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apb15.neorekruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoRekruitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
